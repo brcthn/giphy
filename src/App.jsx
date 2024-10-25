c966bc7d5d6b1ecc844bf129322bab329f1af383
Burcu <burcu@MacBook-Air-von-Burcu.local>
2024-10-25 11:56:09 +0200

    add logo and search input to header

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './Pages/Header';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <p>Hello world</p>
-      </div>
-
-    </>
+    <Header></Header>
   )
 }
 
-export default App
+export default App;
---
+++ src/Pages/Header.jsx
@@ -0,0 +1,38 @@
+
+import IconButton from '@mui/material/IconButton';
+import SearchIcon from '@mui/icons-material/Search';
+import InputAdornment from '@mui/material/InputAdornment';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import { grey } from '@mui/material/colors';
+import Logo from "../assets/logo.png";
+export default function Header() {
+    return (
+        <header class="flex flex-col justify-center items-center p-8 ">
+            <img src={Logo} alt="Giphy Logo" className='max-h-14 max-w-14 pb-3' />
+            <OutlinedInput
+                id="outlined-adornment-weight"
+                endAdornment={<InputAdornment position="end">
+                    <IconButton>
+                        <SearchIcon />
+                    </IconButton>
+                </InputAdornment>}
+                aria-describedby="outlined-weight-helper-text"
+                inputProps={{
+                    'aria-label': 'weight',
+                }}
+                sx={{
+                    width: {
+                        xs: '30ch',
+                        sm: '40ch',
+                        md: '50ch',
+                        lg: '60ch',
+                        xl: '70ch',
+                    },
+                    '&.Mui-focused .MuiOutlinedInput-notchedOutline': {
+                        borderColor: grey[700],
+                    },
+                }}
+            />
+        </header>
+    )
+}
